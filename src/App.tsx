647008ba000aacec3137d88ea56cb763a0c493cf
Sudhanshu Tripathi <sudhanshu.tripathi@1rivet.local>
2019-08-08 19:40:40 +0530

    feat: added required @type libs for react

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: any = () => {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
