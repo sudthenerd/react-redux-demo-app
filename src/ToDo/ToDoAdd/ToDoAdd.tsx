353c45b70c997544248c66adc155e88f64d47f3f
Sudhanshu Tripathi <sudhanshu.tripathi@1rivet.local>
2019-08-20 13:54:32 +0530

    feat: refactor TodoAdd component

--- src/ToDo/ToDoAdd/ToDoAdd.tsx
+++ src/ToDo/ToDoAdd/ToDoAdd.tsx
@@ -2,57 +2,68 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import { connect } from 'react-redux';
-import { addToDoItem } from './middleware/ToDoAddMiddleware';
 // ------------------------------------- //
+import { addToDoItem } from './middleware/ToDoAddMiddleware';
 
 class ToDoAdd extends React.Component<any, any> {
     constructor(props) {
         super(props);
-        console.log(this.props.toDo);
+        this.state = {
+            initialValues: { todoName: (this.props.toDoItem && this.props.editMode) && this.props.toDoItem.name || '' }
+        }
+    }
+
+    private validateForm(values: any) {
+        let errors: any = {};
+
+        if (!values.todoName) {
+            errors.todoName = 'Required';
+        }
+
+        return errors;
+    }
+
+    private onSubmit(values, { setSubmitting, resetForm }) {
+            const editMode: boolean = this.props.editMode;
+            if (editMode) {
+                this.props.addToDo({
+                    index: this.props.toDoItem.index,
+                    name: values.todoName,
+                    editMode: !!editMode
+                });
+            } else {
+                this.props.addToDo({
+                    index:  this.props.todos.length,
+                    name: values.todoName,
+                })
+            }
+            setSubmitting(false);
+            resetForm();
+            this.props.history.push('/todo/list')
+    }
+
+    private renderForm(isSubmitting: any) {
+        return (
+            <Form className="d-flex col-6 align-items-start px-0 py-2">
+                <div className="form-group col px-0">
+                    <Field className="form-control" type="text" name="todoName" placeholder="Enter To Do Item..."/>
+                    <ErrorMessage name="todoName" component="div" />
+                </div>
+                <button className="btn btn-primary ml-3" type="submit" disabled={isSubmitting}>
+                    Add
+                </button>
+            </Form>
+        );
     }
 
     public render() {
         return (
             <div>
               <Formik
-                initialValues={{ todoName: (this.props.toDoItem && this.props.editMode) && this.props.toDoItem.name || ''}}
-                validate={values => {
-                    let errors: any = {};
-                    if (!values.todoName) {
-                        errors.todoName = 'Required';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    const editMode: boolean = this.props.editMode;
-                    if (editMode) {
-                        this.props.addToDo({
-                            index: this.props.toDoItem.index,
-                            name: values.todoName,
-                            editMode: !!editMode
-                        });
-                    } else {
-                        this.props.addToDo({
-                            index:  this.props.todos.length,
-                            name: values.todoName,
-                        })
-                    }
-                    setSubmitting(false);
-                    this.props.history.push('/todo/list')
-                }}
-                >
-                {({ isSubmitting }) => (
-                    <Form className="d-flex col-6 align-items-start px-0 py-2">
-                        <div className="form-group col px-0">
-                            <Field className="form-control" type="text" name="todoName" />
-                            <ErrorMessage name="todoName" component="div" />
-                        </div>
-                        <button className="btn btn-primary ml-3" type="submit" disabled={isSubmitting}>
-                            Add
-                        </button>
-                    </Form>
-                )}
-                </Formik>
+                initialValues={this.state.initialValues}
+                validate={this.validateForm.bind(this)}
+                onSubmit={this.onSubmit.bind(this)}
+                render={({isSubmitting}) => this.renderForm(isSubmitting)} />
             </div>
         )
     }
@@ -61,8 +72,7 @@ class ToDoAdd extends React.Component<any, any> {
 const mapStateToProps: any = (state: any) => ({
     todos: state.toDo.list.todos,
     toDoItem: state.toDo.toDoItem,
-    editMode: state.toDo.editMode,
-    toDo: state.toDo
+    editMode: state.toDo.editMode
 })
 
 const mapDispatchToProps: any = (dispatch: any) => ({
